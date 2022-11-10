d29fcccff6e88a2b56a436a4a9de3d76b2ef48c1
cielv2 <117594215+cielv2@users.noreply.github.com>
2022-11-11 06:23:43 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -51,7 +51,7 @@ export async function handler(chatUpdate) {
                 if (!isNumber(user.exp))
                     user.exp = 0
                 if (!isNumber(user.limit))
-                    user.limit = 100
+                    user.limit = 10
                 if (!isNumber(user.lastclaim))
                     user.lastclaim = 0
                 if (!isNumber(user.pasangan))
